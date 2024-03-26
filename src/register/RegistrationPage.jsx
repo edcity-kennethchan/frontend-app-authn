a61945381e27fb336fb14122cd9d3a65356993e1
mubbsharanwar <mubbshar.anwar@arbisoft.com>
2024-03-26 15:09:54 +0500

    perf: use useSelector with granular approach in registration component to minimize rendrening

--- src/register/RegistrationPage.jsx
+++ src/register/RegistrationPage.jsx
@@ -65,34 +65,25 @@ const RegistrationPage = (props) => {
     institutionLogin,
   } = props;
 
-  const {
-    registrationFormData: backedUpFormData,
-    registrationError,
-    registrationError: {
-      errorCode: registrationErrorCode,
-    } = {},
-    registrationResult,
-    shouldBackupState,
-    userPipelineDataLoaded,
-    submitState,
-    validations,
-  } = useSelector(state => state.register);
+  const backedUpFormData = useSelector(state => state.register.registrationFormData);
+  const registrationError = useSelector(state => state.register.registrationError);
+  const registrationErrorCode = registrationError?.errorCode;
+  const registrationResult = useSelector(state => state.register.registrationResult);
+  const shouldBackupState = useSelector(state => state.register.shouldBackupState);
+  const userPipelineDataLoaded = useSelector(state => state.register.userPipelineDataLoaded);
+  const submitState = useSelector(state => state.register.submitState);
+  const validations = useSelector(state => state.register.validations);
 
-  const {
-    fieldDescriptions,
-    optionalFields,
-    thirdPartyAuthApiStatus,
-    thirdPartyAuthContext,
-    thirdPartyAuthContext: {
-      autoSubmitRegForm,
-      errorMessage: thirdPartyAuthErrorMessage,
-      finishAuthUrl,
-      currentProvider,
-      providers,
-      secondaryProviders,
-      pipelineUserDetails,
-    },
-  } = useSelector(state => state.commonComponents);
+  const fieldDescriptions = useSelector(state => state.commonComponents.fieldDescriptions);
+  const optionalFields = useSelector(state => state.commonComponents.optionalFields);
+  const thirdPartyAuthApiStatus = useSelector(state => state.commonComponents.thirdPartyAuthApiStatus);
+  const autoSubmitRegForm = useSelector(state => state.commonComponents.thirdPartyAuthContext.autoSubmitRegForm);
+  const thirdPartyAuthErrorMessage = useSelector(state => state.commonComponents.thirdPartyAuthContext.errorMessage);
+  const finishAuthUrl = useSelector(state => state.commonComponents.thirdPartyAuthContext.finishAuthUrl);
+  const currentProvider = useSelector(state => state.commonComponents.thirdPartyAuthContext.currentProvider);
+  const providers = useSelector(state => state.commonComponents.thirdPartyAuthContext.providers);
+  const secondaryProviders = useSelector(state => state.commonComponents.thirdPartyAuthContext.secondaryProviders);
+  const pipelineUserDetails = useSelector(state => state.commonComponents.thirdPartyAuthContext.pipelineUserDetails);
 
   const backendValidations = useMemo(
     () => getBackendValidations(registrationError, validations), [registrationError, validations],
@@ -130,7 +121,9 @@ const RegistrationPage = (props) => {
       }
     }
   }, [ // eslint-disable-line react-hooks/exhaustive-deps
-    thirdPartyAuthContext,
+    thirdPartyAuthApiStatus,
+    thirdPartyAuthErrorMessage,
+    pipelineUserDetails,
     userPipelineDataLoaded,
   ]);
 
